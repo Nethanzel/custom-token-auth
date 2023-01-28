66ba8c30a4e920b8ea710acbe7b2f29027868085
Nethanzel <natanaelabaad@gmail.com>
2023-01-28 12:45:14 -0400

    Update from some unknown issue

--- src/Auth/Utils/hashModule.js
+++ src/Auth/Utils/hashModule.js
@@ -3,17 +3,21 @@ const algorithm = "aes-256-ctr";
 const initVector = Crypto.randomBytes(16);
 const passphrase = process.env.HASHING_KEY; /* hashing key must have exactly 32 characters */
 
-
 function encryptWithAES(text) {
-  const cipher = Crypto.createCipheriv(algorithm, passphrase, initVector);
-  let encryptedData = cipher.update(text, "utf-8", "hex") + cipher.final("hex");
-  return encryptedData;
+  let cipher = Crypto.createCipheriv(algorithm, Buffer.from(passphrase), initVector);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return `${encrypted.toString('base64')}@${initVector.toString('base64')}`;
 };
 
 function decryptWithAES (ciphertext) {
-  const decipher = Crypto.createDecipheriv(algorithm, passphrase, initVector);
-  let decryptedData = decipher.update(ciphertext, "hex", "utf-8") + decipher.final("utf8");
-  return decryptedData;
+  let preProcess = ciphertext.split("@");
+  let iv = Buffer.from(preProcess[1], 'base64');
+  let encryptedText = Buffer.from(preProcess[0], 'base64');
+  let decipher = Crypto.createDecipheriv(algorithm, Buffer.from(passphrase), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 };
 
 module.exports.decryptWithAES = decryptWithAES;
